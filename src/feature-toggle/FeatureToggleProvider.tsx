0ff131fa1d260c7465b76d4ff8d36862c1d9988e
Nat-macbook <nattha.int@gmail.com>
2023-03-03 17:16:26 +0700

    Cleaning up codes

--- src/feature-toggle/FeatureToggleProvider.tsx
+++ src/feature-toggle/FeatureToggleProvider.tsx
@@ -4,14 +4,18 @@ type MyAppFeatureToggle = {
   CH1025: boolean
 }
 
-export const FeatureToggleContext = createContext<MyAppFeatureToggle>({CH1025: false})
+export const FeatureToggleContext =
+  createContext<MyAppFeatureToggle>({CH1025: false})
 
 type Props = {
   children: React.ReactElement
   featureToggle: MyAppFeatureToggle
 }
 
-export const FeatureToggleProvider: React.FC<Props> = ({children, featureToggle}): React.ReactElement =>
+export const FeatureToggleProvider: React.FC<Props> = ({
+  children,
+  featureToggle
+}): React.ReactElement =>
   <FeatureToggleContext.Provider value={featureToggle}>
     {children}
   </FeatureToggleContext.Provider>
