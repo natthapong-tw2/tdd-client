0e80a61e8744b9c8d79be7b8d1a8f7b87632dcff
Nat-macbook <nattha.int@gmail.com>
2023-03-03 17:14:59 +0700

    Implement feature toggle by ticket number

--- src/feature-toggle/FeatureToggleProvider.test.tsx
+++ src/feature-toggle/FeatureToggleProvider.test.tsx
@@ -1,26 +1,26 @@
-import {render} from "@testing-library/react";
-import React, {createContext} from "react";
+import {render, screen} from "@testing-library/react";
+import React from "react";
+import {FeatureToggleProvider, useFeatureToggle} from "./FeatureToggleProvider";
 
-type MyAppFeatureToggle = {
-  CH1025: boolean
+const TestComponent: React.FC = (): React.ReactElement => {
+  const { CH1025 } = useFeatureToggle()
+  return CH1025 ? <>Feature toggle is on</> : <>Under construction</>;
 }
-
-const FeatureToggleContext = createContext<MyAppFeatureToggle>({ CH1025: false })
-
-type Props = {
-  children: React.ReactElement
-}
-
-const FeatureToggleProvider: React.FC<Props> = ({children}): React.ReactElement =>
-  <FeatureToggleContext.Provider value={{ CH1025: false }}>
-    { children }
-  </FeatureToggleContext.Provider>
-
-const TestComponent: React.FC = (): React.ReactElement => <></>
 
 describe("FeatureToggleProvider", () => {
-  it("should be able to render", () => {
-    render(<FeatureToggleProvider><TestComponent/></FeatureToggleProvider>)
-    expect(true).toEqual(true)
+  it("should render under construction when feature toggle is off", () => {
+    render(<FeatureToggleProvider featureToggle={{ CH1025: false }}>
+      <TestComponent/>
+    </FeatureToggleProvider>)
+
+    expect(screen.getByText(/Under construction/)).toBeInTheDocument()
+  })
+
+  it("should render feature toggle on when feature toggle is on", () => {
+    render(<FeatureToggleProvider featureToggle={{ CH1025: true }}>
+      <TestComponent/>
+    </FeatureToggleProvider>)
+
+    expect(screen.getByText(/Feature toggle is on/)).toBeInTheDocument()
   })
 })
---
+++ src/feature-toggle/FeatureToggleProvider.tsx
@@ -0,0 +1,19 @@
+import React, {createContext, useContext} from "react";
+
+type MyAppFeatureToggle = {
+  CH1025: boolean
+}
+
+export const FeatureToggleContext = createContext<MyAppFeatureToggle>({CH1025: false})
+
+type Props = {
+  children: React.ReactElement
+  featureToggle: MyAppFeatureToggle
+}
+
+export const FeatureToggleProvider: React.FC<Props> = ({children, featureToggle}): React.ReactElement =>
+  <FeatureToggleContext.Provider value={featureToggle}>
+    {children}
+  </FeatureToggleContext.Provider>
+
+export const useFeatureToggle = () => useContext(FeatureToggleContext)
